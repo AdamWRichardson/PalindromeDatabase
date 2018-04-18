635294503c24ea31cd6e0f7ec171b4975fb085e3
AdamWRichardson <awr.trumpet@gmail.com>
2018-04-18 11:34:19 +0100

    First commit of this iteration to of the palindrome database

---
+++ PalDB/Check.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python2.7
+__author__ = "Adam Richardson"
+__Date__ = 18 / 04 / 18
+
+# Move the request import into the file where it's needed
+from flask import request
+
+# Remove all punctuation from users string
+punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''' '
+
+
+def check(list_of_palindromes):
+    # Need a way of taking the user input, from there check if i   t's a palindrome
+    # String input in request forms is the name of the box in the html file home.html
+    word = str(request.form['palindromes'])
+    no_punc = ''
+    for char in word:
+        if char not in punc:
+            no_punc = (no_punc + char).lower()
+    true = no_punc == no_punc[::-1]
+    if true:
+        list_of_palindromes.append(word)
+        return true
---
+++ PalDB/Pal.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python2.7
+__author__ = "Adam Richardson"
+__Date__ = 18 / 04 / 18
+
+import os
+from flask import Flask, render_template, flash, redirect, url_for
+from Check import *
+
+# This initiates an instance of the app
+app = Flask(__name__)
+app.config.from_object(__name__)  # load config from this file , palDB.py
+
+# Load default config and override config from an environment variable
+# Need to add a secret key otherwise it doesn't know you're unique and throws up errors
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'palDB.db'),
+    SECRET_KEY='development key',
+))
+app.config.from_envvar('PALDB_SETTINGS', silent=True)
+
+# Create a list of palindromes that the user has entered
+list_pal = []
+
+
+# This is then how you specify different routes within the web pages
+@app.route('/')
+def home():
+    return render_template('home.html', palindromes=list_pal)
+
+
+# Add a route which redirects either adding to the table or removing from table
+@app.route('/add', methods=['POST'])
+def addPal():
+    goodPal = check(list_pal)
+    if goodPal:
+        flash('New palindrome accepted')
+        return redirect(url_for('home'))
+    else:
+        flash('Sorry, your word was not a palindrome')
+        return redirect(url_for('home'))
+
+
+def data():
+    return
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
